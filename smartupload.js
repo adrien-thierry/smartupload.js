8f3ece24ec3ea81c31bee3b713a4b3126e29e77e
Hykios42 <jimmy.charpagne@gmail.com>
2015-12-23 17:56:03 +0100

    fix update

--- smartupload.js
+++ smartupload.js
@@ -1,19 +1,35 @@
 $(function smartUpload() {
 
-	var fileToUpload =  ;
+/*	Obj to upload */
 
 	var smartUploadObject = {
-		SMARTSIZE:  1048576,
-		TARGET: createObjectURL(this); // add here the url you target
+		var reader,
+		var SMARTSIZE = 1048576,
+		var TARGET = document.querySelector('file'),
 
 	}
 
-	if (fileToUpload >= SMARTSIZE) {
-
+	function error() {
+		console.log("error");
 	}
-	else (fileToUpload < SMARTSIZE) {
 
+	function abortReadding() {
+		console.log("Read abort");
+		reader.abort();
 	}
+
+	function uploadFile() {
+
+		var sizeMax = 128000;
+		var sizeMin = 100000;
+		if (TARGET.Blob.size < sizeMax) 
+			console.log("File too big");
+		else if (TARGET.Blob.size < sizeMax && TARGET.Blob.size > sizeMin)
+			// create a request for an empty file
+			// split and send it by buffer[sizeMin] 
+	}
+
+
 
 }
 
