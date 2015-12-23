17a90c14758648f243982bd33f14914ba09d963a
Hykios42 <jimmy.charpagne@gmail.com>
2015-12-23 17:48:53 +0100

    add js file, work in progress

---
+++ smartupload.js
@@ -0,0 +1,19 @@
+$(function smartUpload() {
+
+	var fileToUpload =  ;
+
+	var smartUploadObject = {
+		SMARTSIZE:  1048576,
+		TARGET: createObjectURL(this); // add here the url you target
+
+	}
+
+	if (fileToUpload >= SMARTSIZE) {
+
+	}
+	else (fileToUpload < SMARTSIZE) {
+
+	}
+
+}
+
